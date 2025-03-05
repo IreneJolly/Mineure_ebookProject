44268ee6068f957895cdefa904131553bc7d66a1
IreneJolly <irene.jollypeckre@gmail.com>
2025-03-05 14:08:19 +0100

    feat: ajout et suppression de chapitre

--- app/page.js
+++ app/page.js
@@ -1,18 +1,21 @@
 "use client";
 
 import { useState } from "react";
-import dynamic from "next/dynamic"; // Import dynamic from next/dynamic  
-import '../styles/global.css'; // Import du fichier CSS global
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic
+import "../styles/global.css"; // Import du fichier CSS global
 
 let options = require("../public/livre.json");
 
-const QuillEditor = dynamic(() => import('../component/QuillEditor'), {
-  ssr: false, // This ensures the QuillEditor is only rendered on the client  
+const QuillEditor = dynamic(() => import("../component/QuillEditor"), {
+  ssr: false, // This ensures the QuillEditor is only rendered on the client
 });
 
 export default function Home() {
-  const [message, setMessage] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0); // État pour suivre l'index actuel
+
+  const handleSelectPage = (index) => {
+    setCurrentIndex(index);
+  };
 
   const handleRunTests = async () => {
     try {
@@ -21,34 +24,36 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(options), // Send the modified options in the request body  
+        body: JSON.stringify(options), // Send the modified options in the request body
       });
-
-      if (!response.ok) {
-        throw new Error("Erreur lors de l'enregistrement du fichier JSON");
-      }
-
-      const response1 = await fetch("../api/run-test", {
-        method: "POST",
-      });
-
-      setMessage("Fichier téléchargé !");
     } catch (error) {
       console.error("Erreur lors de l'exécution des tests :", error);
-      setMessage("Erreur lors de l'exécution des tests.");
     }
   };
 
-  // Fonction pour gérer le changement de page  
-  const handleSelectPage = (index) => {
-    setCurrentIndex(index); // Met à jour l'index courant  
+  const handleAddChapter = () => {
+    options.content.push({ title: "Chapitre :", data: "" });
+    handleRunTests();
+  };
+
+  const handleDeleteChapter = () => {
+    console.log(currentIndex);
+    options.content.splice(currentIndex, 1);
+    console.log(options.content);
+    handleRunTests();
+  };
+
+  const handleTitleChange = () => {
+    options.content[currentIndex].title = document.getElementById("title").value;
+    document.getElementById("title").value = options.content[currentIndex].title;
+    handleRunTests();
   };
 
   return (
     <div className="h-full">
       <div className="container">
         <div className="sidebar">
-          <h2>Liste des Pages</h2>
+          <h2>Liste des Chapitres</h2>
           <ul>
             {options.content.map((item, index) => (
               <li
@@ -59,18 +64,45 @@ export default function Home() {
               </li>
             ))}
           </ul>
+          <button
+            onClick={handleAddChapter}
+            style={{ fontSize: "24px", cursor: "pointer" }}
+          >
+            +
+          </button>
+          <button
+            onClick={handleDeleteChapter}
+            style={{ background: "none", border: "none", cursor: "pointer" }}
+          >
+            <img
+              src="delete.png"
+              alt="Supprimer"
+              style={{ width: "24px", height: "24px" }}
+            />
+          </button>
         </div>
         <div className="editor-container">
-          <link rel="stylesheet" href="https://cdn.quilljs.com/1.3.6/quill.snow.css" />
-          <h3>{options.content[currentIndex]?.title || "Sélectionnez un chapitre"}</h3> {/* Affiche le titre du chapitre sélectionné */}
+          <link
+            rel="stylesheet"
+            href="https://cdn.quilljs.com/1.3.6/quill.snow.css"
+          />
+          <h3>{options.content[currentIndex]?.title || "Sélectionnez un chapitre"}</h3>
+          <input
+            type="text"
+            id="title"
+            defaultValue={options.content[currentIndex].title}
+          />
+          <button onClick={handleTitleChange}>Modifier</button>
           <QuillEditor
-            initialContent={options.content[currentIndex]?.data || ""} // Passer le contenu initial  
+            initialContent={options.content[currentIndex]?.data || ""}
+            index={currentIndex}
           />
         </div>
       </div>
     </div>
   );
 }
+
 /* Idee : 
 - Faire des boutons pour envoyer d'une page vers une autre
 - authentification pour continuer la modification du fichier sauvegarder temporairement sur supabase
--- component/QuillEditor.js
+++ component/QuillEditor.js
@@ -1,34 +1,60 @@
-import React, { useEffect, useRef } from 'react';
-import Quill from 'quill';
+import React, { useEffect, useRef, useState } from "react";
+import Quill from "quill";
 
-const QuillEditor = ({ initialContent}) => {
-    const editorRef = useRef(null);
-    const quillRef = useRef(null);
+let options = require("../public/livre.json");
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            if (!quillRef.current) {
-                quillRef.current = new Quill(editorRef.current, {
-                    theme: 'snow',
-                    modules: {
-                        toolbar: [
-                            [{ 'header': [1, 2, false] }],
-                            ['bold', 'italic', 'underline'],
-                            ['image', 'code-block'],
-                            [{ 'list': 'ordered' }, { 'list': 'bullet' }]
-                        ],
-                    },
-                });
-            }
-            quillRef.current.clipboard.dangerouslyPasteHTML(initialContent);
-        }
-    }, [initialContent]);
+const QuillEditor = ({ initialContent, index }) => {
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
 
-    return (
-        <div>
-            <div ref={editorRef} style={{ height: '400px', border: '1px solid #ccc' }}></div>
-        </div>
-    );
+  const handleRunTests = async () => {
+    try {
+      const response = await fetch("../api/writeJson", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(options), // Send the modified options in the request body
+      });
+    } catch (error) {
+      console.error("Erreur lors de l'exécution des tests :", error);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!quillRef.current) {
+        quillRef.current = new Quill(editorRef.current, {
+          theme: "snow",
+          modules: {
+            toolbar: [
+              [{ header: [1, 2, false] }],
+              ["bold", "italic", "underline"],
+              ["image", "code-block"],
+              [{ list: "ordered" }, { list: "bullet" }],
+            ],
+          },
+        });
+
+        quillRef.current.on("text-change", () => {
+          console.log(options.content[index].data);
+          options.content[index].data = quillRef.current.root.innerHTML;
+          handleRunTests();
+        });
+      }
+
+      quillRef.current.clipboard.dangerouslyPasteHTML(initialContent);
+    }
+  }, [initialContent, index]);
+
+  return (
+    <div>
+      <div
+        ref={editorRef}
+        style={{ height: "400px", border: "1px solid #ccc" }}
+      ></div>
+    </div>
+  );
 };
 
 export default QuillEditor;
